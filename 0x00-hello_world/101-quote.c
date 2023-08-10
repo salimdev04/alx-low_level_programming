48ae633761745deafc039778d8bcad0e5dc2f690
salimdev04 <salimdev04@gmail.com>
2023-08-10 16:21:42 -0700

    corr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,8 @@
 #include <unistd.h>
 
-/*
+/**
  * main - Entry point
- *
- * Description: 'print string using write function'
- *
+ * Description: 'print string using write function
  * Return: Always 1 if success
  */
 int main(void)
