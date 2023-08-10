698b059f04caed381adb53a2afb4814b3675163c
salimdev04 <salimdev04@gmail.com>
2023-08-10 09:08:12 -0700

    advanced ccommit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdlib.h>
+
+/*
+ * main - Entry point
+ * Description: 'writting a quotes'
+ * Return: Always 1 if success
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: use of sizeof functions
+ * Return: Always 0 (success)
+ */
+int main(void) {
+printf("Size of char: %zu bytes\n", sizeof(char));
+printf("Size of short: %zu bytes\n", sizeof(short));
+printf("Size of int: %zu bytes\n", sizeof(int));
+printf("Size of long: %zu bytes\n", sizeof(long));
+printf("Size of long long: %zu bytes\n", sizeof(long long));
+printf("Size of float: %zu bytes\n", sizeof(float));
+printf("Size of double: %zu bytes\n", sizeof(double));
+printf("Size of long double: %zu bytes\n", sizeof(long double));
+return (0);
+}
