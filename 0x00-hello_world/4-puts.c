fe972c30364b41228ff6781c1d4559685f9de097
salimdev04 <salimdev04@gmail.com>
2023-08-10 08:38:05 -0700

    correction5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * puts functions
+ * return: Always 0 (success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");    
-	return (0);
+    puts("\"Programming is like building a multilingual puzzle");    
+    return (0);
 }
 
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * printf functions
+ * return: Always 0 (success)
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");    
-	return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");    
+    return (0);
 }
 
