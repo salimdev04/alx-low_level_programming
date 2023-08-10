7325aa1e69001a3444ddb5fd80044073d1514a5c
salimdev04 <salimdev04@gmail.com>
2023-08-10 15:24:32 -0700

    correction9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,14 @@
 #include <unistd.h>
 
+/*
+ * main - Entry point
+ * Description: 'print string using write function
+ * Return: Always 1 if success
+ * */
 int main(void)
 {
-    char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    size_t len = sizeof(str) - 1;
-    write(STDOUT_FILENO, str, len);
-    return (1);
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+size_t len = sizeof(str) - 1;
+write(STDOUT_FILENO, str, len);
+return (1);
 }
