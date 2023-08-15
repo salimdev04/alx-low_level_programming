b9d2c147c777f4dec4c2298ed59e7c7afd587a7a
salimdev04 <salimdev04@gmail.com>
2023-08-15 02:46:04 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'print _putchar'
+ * Return: Always 0
+ */
+int main(void)
+{
+	char word[] = "_putchar";
+	
+	for (int w = 0; w < 8; w++)
+	{
+		putchar(word[w]);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+ * Description: 'print _putchar'
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	for (int w = 'a'; w <= 'z'; w++)
+	{
+		putchar(w);
+	}
+	putchar('\n');
+}
