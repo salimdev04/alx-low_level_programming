a571e338811ea4f49872d07d49a017f8cdfea706
salimdev04 <salimdev04@gmail.com>
2023-08-16 10:50:31 -0700

    coorrr

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,53 +3,64 @@
 #include<string.h>
 #include "main.h"
 
-void printInteger(int num) {
+/**
+ * printInteger - Entry point
+ * Description: 'printing using _putchar'
+ * @num: 'an integer'
+ * Return: void
+ */
+void printInteger(int num)
+{
 	int i;
 
-    if (num < 0) {
-        putchar('-');
-        num = -num;
-    }
+	if (num < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if (num == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		char digits[10];
+		int count = 0;
 
-    if (num == 0) {
-        putchar('0');
-    } else {
-        char digits[10];
-        int count = 0;
-
-        while (num > 0) {
-            digits[count] = num % 10 + '0';
-            num /= 10;
-            count++;
-        }
-
-        for (i = count - 1; i >= 0; i--) {
-            putchar(digits[i]);
-        }
-    }
-    if (num < 98)
-    {
-	putchar(',');
-	putchar(' ');
+		while (num > 0)
+		{
+			digits[count] = num % 10 + '0';
+			num /= 10;
+			count++;
+		}
+		for (i = count - 1; i >= 0; i--)
+		{
+			_putchar(digits[i]);
+		}
 	}
 }
 
 /**
  * print_to_98 - Entry point
  *
- * Description: 'print _putchar'
+ * Description: 'print __putchar'
  * @a: 'an integer'
  * Return: 1 if true and 0 if false
  */
 void print_to_98(int a)
 {
 	int i;
-	
+
 	if (a < 98)
 	{
 		for (i = a; i <= 98; i++)
 		{
 			printInteger(i);
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
 	else
@@ -57,7 +68,12 @@ void print_to_98(int a)
 		for (i = a; i >= 98; i--)
 		{
 			printInteger(i);
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
-	putchar('\n');
+	_putchar('\n');
 }
