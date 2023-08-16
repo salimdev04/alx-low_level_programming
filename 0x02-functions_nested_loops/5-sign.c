af16316db9d7e989d9d769101697c694bb06be87
salimdev04 <salimdev04@gmail.com>
2023-08-16 03:41:20 -0700

    correcct5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,16 +15,18 @@ int print_sign(int c)
 	if (c > 0)
 	{
 		putchar('+');
-		return (1);
+		putchar('1');
 	}
 	else if (c == 0)
 	{
 		putchar('0');
-		return (0);
+		putchar('0');
 	}
 	else
 	{
 		putchar('-');
-		return (-1);
+		putchar('1');
 	}
+
+	return (1);
 }
