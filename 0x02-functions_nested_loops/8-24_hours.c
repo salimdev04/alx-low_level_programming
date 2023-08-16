f3e8565a97a8f99440f36b5d5cefaa00851c0712
salimdev04 <salimdev04@gmail.com>
2023-08-16 12:26:28 -0700

    Im the best2

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,57 +3,37 @@
 #include "main.h"
 
 /**
- * printInteger - Entry point
- * Description: 'printing using _putchar'
- * @num: 'an integer'
+ * handle_putchar - Entry point
+ * Description: printing
+ * @h0: hours 0
+ * @h1: hours 1
+ * @min0: minut 0
+ * @min1: minut 1
  * Return: void
  */
-void printInteger(int num)
+void handle_putchar(int h0, int h1, int min0, int min1)
 {
-	int i;
-
-	if (num < 0)
-	{
-		_putchar('-');
-		num = -num;
-	}
-	if (num == 0)
-	{
-		_putchar('0');
-	}
-	else
-	{
-		char digits[10];
-		int count = 0;
-
-		while (num > 0)
-		{
-			digits[count] = num % 10 + '0';
-			num /= 10;
-			count++;
-		}
-		for (i = count - 1; i >= 0; i--)
-		{
-			_putchar(digits[i]);
-		}
-	}
+	_putchar('0' + h0);
+	_putchar('0' + h1);
+	_putchar(':');
+	_putchar('0' + min0);
+	_putchar('0' + min1);
+	_putchar('\n');
 }
 
 /**
  * jack_bauer - Entry point
- *
  * Description: 'print _putchar'
- *
  * Return: 1 if true and 0 if false
  */
 void jack_bauer(void)
 {
 	int hour_0, hour_1, minut_0, minut_1;
+
 	hour_0 = 0;
 	hour_1 = 0;
 	minut_0 = 0;
 	minut_1 = 0;
-
 	while (hour_1 <= 2)
 	{
 		while (hour_0 <= 9)
@@ -62,12 +42,7 @@ void jack_bauer(void)
 			{
 				while (minut_0 <= 9)
 				{
-					_putchar('0' + hour_1);
-					_putchar('0' + hour_0);
-					_putchar(':');
-					_putchar('0' + minut_1);
-					_putchar('0' + minut_0);
-					_putchar('\n');
+					handle_putchar(hour_1, hour_0, minut_1, minut_0);
 
 					if (hour_1 == 2 && hour_0 == 3 && minut_1 == 5 && minut_0 == 9)
 					{
