722c4f58c27cc4b95210ce53f189e6b0aa6b6b96
salimdev04 <salimdev04@gmail.com>
2023-08-17 11:00:11 -0700

    (0x04) task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar('0' + i);
+		}
+	}
+	_putchar('\n');
+}
