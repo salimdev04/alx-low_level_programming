75b2eda4ce140165595caf8dfcd0a1b591820667
salimdev04 <salimdev04@gmail.com>
2023-08-17 10:46:44 -0700

    (0x04) task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: integer
+ * @b: integer
+ * Return: 1 if true and 0 if false
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
