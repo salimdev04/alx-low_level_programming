078830279f2d319777cbd0567b013554fd2354d6
salimdev04 <salimdev04@gmail.com>
2023-08-17 10:41:30 -0700

    (0x04) task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ * @c: integer
+ * Return: 1 if true and 0 if false
+ */
+int _isdigit(int c)
+{
+	if (isdigit(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
