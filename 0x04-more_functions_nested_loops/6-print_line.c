f657e2b71f942d48353fd1770a95a0f4ee3e0b4f
saimdev <60602690+salimdev04@users.noreply.github.com>
2023-08-19 20:44:49 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,21 +1,22 @@
-#include<stdio.h>
-#include <ctype.h>
-#include "main.h"
+#include"main.h"
 
 /**
- * print_line - Entry point
- * @n: integer
- */
+ * print_line - print a straight line
+ *
+ * @n: is the number of times the _ character
+ *     should be printed
+*/
+
 void print_line(int n)
 {
 	int i;
 
-	if (n > 0)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
-		for (i = 0; i <= n; i++)
-		{
+		for (i = 1; i <= n; ++i)
 			_putchar('_');
-		}
+		_putchar('\n');
 	}
-	putchar('\n');
 }
